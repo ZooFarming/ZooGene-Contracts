c2d01bd7dac288e93c30621efb0dbf9f2129e41c
lolieatapple <lolieatapple@gmail.com>
2024-12-21 22:51:31 +0800

    update zoo genes v2 contract

---
+++ contracts/ZooGeneV2.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ZooGene is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    constructor(address initialOwner)
+        ERC721("ZooGene", "ZooGene")
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to, uint256 tokenId, string memory uri)
+        public
+        onlyOwner
+    {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function safeBatchMint(address[] memory users, uint256[] memory tokenIds, string[] memory uris)
+        public
+        onlyOwner
+    {
+        for (uint256 i = 0; i < users.length; i++) {
+            _safeMint(users[i], tokenIds[i]);
+            _setTokenURI(tokenIds[i], uris[i]);
+        }
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
